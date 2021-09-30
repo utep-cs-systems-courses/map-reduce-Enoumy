364499483a34d418fdd8f4d7dc542a168b740264
enoumy <enoumy@gmail.com>
2021-09-29 23:47:07 -0600

    Added more granular timing

---
+++ run_with_granular_timing.sh
@@ -0,0 +1,5 @@
+python3 solution.py --no_pymp True --silent True --time_file_reading True --time_counting True
+python3 solution.py --num_threads 1 --silent True --time_file_reading True --time_counting True
+python3 solution.py --num_threads 2 --silent True --time_file_reading True --time_counting True
+python3 solution.py --num_threads 4 --silent True --time_file_reading True --time_counting True
+python3 solution.py --num_threads 8 --silent True --time_file_reading True --time_counting True
--- solution.py
+++ solution.py
@@ -11,6 +11,24 @@ import argparse
 import pymp
 import time
 
+expected_dictionary = {
+    "hate": 332,
+    "love": 3070,
+    "death": 1016,
+    "night": 1402,
+    "sleep": 470,
+    "time": 1806,
+    "henry": 661,
+    "hamlet": 475,
+    "you": 23306,
+    "my": 14203,
+    "blood": 1009,
+    "poison": 139,
+    "macbeth": 288,
+    "king": 4545,
+    "heart": 1458,
+    "honest": 434,
+}
 
 files_to_search = [
     'shakespeare1.txt',
@@ -43,12 +61,19 @@ words_to_count = [
     'honest',
 ]
 
+TIME_FILE_READING = False
+TIME_WORD_COUNTING = False
+
 
 def clean_word(word):
     """
     Removes any weird punctuation inside of a word
     """
     return ''.join(c for c in word if c.isalpha()).lower()
+
+
+def gettime():
+    return time.clock_gettime(time.CLOCK_MONOTONIC)
 
 
 def count_words_from_file(filename):
@@ -58,11 +83,21 @@ def count_words_from_file(filename):
     """
     out = {word: 0 for word in words_to_count}
 
+    start = gettime()
     with open(filename, 'r') as opened_file:
-        for line in opened_file:
-            line = line.lower()
-            for word in words_to_count:
-                out[word] += line.count(word)
+        contents = opened_file.read().lower()
+        time_to_read = gettime() - start
+
+        start = gettime()
+        for word in words_to_count:
+            out[word] += contents.count(word)
+        time_to_count = gettime() - start
+
+    if TIME_FILE_READING:
+        print(f'{filename} time to read file: {time_to_read}')
+
+    if TIME_WORD_COUNTING:
+        print(f'{filename} time to count words: {time_to_count}')
 
     return out
 
@@ -95,7 +130,7 @@ def compute_with_map_reduce(num_threads):
             current_counts = count_words_from_file(filename)
 
             # Reducing the multiple dictionaries into a single aggregate
-            # dictionry
+            # dictionary
             sum_lock.acquire()
             add_dictionaries(current_counts, shared_dict)
             sum_lock.release()
@@ -103,7 +138,20 @@ def compute_with_map_reduce(num_threads):
     return dict(shared_dict)
 
 
+def verify(results):
+    for key in results:
+        if key not in expected_dictionary or results[key] != expected_dictionary[key]:
+            assert False, 'Results did not match expected results'
+
+    for key in expected_dictionary:
+        if key not in results or results[key] != expected_dictionary[key]:
+            assert False, 'Results did not match expected results'
+
+
 def main():
+    global TIME_FILE_READING
+    global TIME_WORD_COUNTING
+
     parser = argparse.ArgumentParser(
         description="Map Reduce assignment. Counts words inside of Shakespeare's works!")
     parser.add_argument('--silent', default=False, type=bool,
@@ -112,19 +160,28 @@ def main():
                         help="Number of  threads which will be created in order to count the number of words.")
     parser.add_argument('--no_pymp', default=False, type=bool,
                         help="If set to true, a synchronous version that does not use pymp will run.")
+    parser.add_argument('--time_file_reading', default=False, type=bool,
+                        help="If set to true, this will time the file reading operation")
+    parser.add_argument('--time_counting', default=False, type=bool,
+                        help="If set to true, this will time the counting operations")
     args = parser.parse_args()
 
-    start = time.clock_gettime(time.CLOCK_MONOTONIC)
+    TIME_FILE_READING = args.time_file_reading
+    TIME_WORD_COUNTING = args.time_counting
+
+    start = gettime()
     if args.no_pymp:
         counts = compute_synchronously()
     else:
         counts = compute_with_map_reduce(args.num_threads)
-    time_taken = time.clock_gettime(time.CLOCK_MONOTONIC) - start
+    time_taken = gettime() - start
 
     print(
         f"(pymp: {not args.no_pymp}) (# threads: {args.num_threads}) Time taken: {time_taken}")
     if not args.silent:
         print(f"Counts: {counts}")
+
+    verify(counts)
 
 
 if __name__ == '__main__':
